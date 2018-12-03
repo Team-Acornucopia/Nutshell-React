e1f776802ea4c1acb82e6ca667075a61efce9d0d
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-12-03 13:20:35 -0600

    still working on some styling for event prominence

--- src/components/events/EventsCard.js
+++ src/components/events/EventsCard.js
@@ -7,20 +7,25 @@ export default class EventsCard extends Component {
 
   render() { 
     let prominent = ""
+    let cardColor = ""
     if (this.props.highlight) {
       prominent="prominent card-title"
+      cardColor="green"
     } else {
       prominent = "card-title"
+      cardColor= null
     }
+
+    
       return (
           <Card.Group>
 
-              <Card key={this.props.evt.id} className="card">
+              <Card key={this.props.evt.id} className="card" color={cardColor}>
                   <Card.Content className="card-body">
                       <Card.Header className={prominent}>Event: {this.props.evt.name}</Card.Header>
-                          <Card.Meta className={prominent}>Date: {this.props.evt.date} </Card.Meta>
-                          <Card.Description className={prominent}>Time: {this.props.evt.time}</Card.Description>
-                          <Card.Description className={prominent}>Location: {this.props.evt.location}</Card.Description>
+                          <Card.Meta>Date: {this.props.evt.date} </Card.Meta>
+                          <Card.Description>Time: {this.props.evt.time}</Card.Description>
+                          <Card.Description>Location: {this.props.evt.location}</Card.Description>
                           <Button as={Link} size="tiny" color="purple" className="nav-link" to={`/events/${this.props.evt.id}`}>Details</Button>
                         <Button as={Link} size="tiny" color="orange" className="card-link" to={`/events/edit/${this.props.evt.id}`}>Edit</Button>
                         <Button size="tiny" color="red"
