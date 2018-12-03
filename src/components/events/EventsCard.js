ba838bb9b2d29afc5b0a9e98884bbf4e79919467
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-12-03 14:57:09 -0600

    fixed minor bug

--- src/components/events/EventsCard.js
+++ src/components/events/EventsCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { Button, Card, Image } from 'semantic-ui-react'
 import { Link } from "react-router-dom";
-import './Events.css'
+
 
 export default class EventsCard extends Component {
 
