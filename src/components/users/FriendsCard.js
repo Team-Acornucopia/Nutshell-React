c423bb83ecdf8fd7e45589a158400fc0d7ee4c42
Zac Jones <zacjones91@gmail.com>
2018-12-04 10:23:46 -0600

    changed messages to cards on users and friends

--- src/components/users/FriendsCard.js
+++ src/components/users/FriendsCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Button, Message, Image, Header, Modal } from "semantic-ui-react";
+import { Button, Image, Header, Modal, Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import "./Friends.css"
+import "./Friends.css";
 
 export default class FriendsCard extends Component {
   state = { open: false };
@@ -32,60 +32,67 @@ export default class FriendsCard extends Component {
     const { open, dimmer } = this.state;
     return (
       <React.Fragment>
-        <Message key={this.props.user.id} className="card">
-          <h5 className="card-title">
-            <Message.Header>
-              Friend's Name: {this.props.user.username}
-            </Message.Header>
-            <Button
-              size="tiny"
-              color="red"
-              onClick={() =>
-                this.props.deleteFriend(thisSpecificFriend, currentUser)
-              }
-              className="card-link"
-            >
-              Delete Friend
-            </Button>
-            <Modal
-              dimmer={dimmer}
-              open={open}
-              onClose={this.close}
-              trigger={
-                <Button size="tiny" color="purple" onClick={this.show(true)}>
-                  Details
-                </Button>
-              }
-            >
-              <Modal.Header>Details</Modal.Header>
-              <Modal.Content image>
-                <Image
-                  wrapped
-                  size="small"
-                  src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
-                />
-                <Modal.Description>
-                  <Header>{this.props.user.username}</Header>
-                  <p>This person is your friend!</p>
-                </Modal.Description>
-              </Modal.Content>
-              <Modal.Actions>
-                <Button onClick={this.close} positive>
-                  Back
-                </Button>
-                <Button
-                  onClick={() =>
-                    this.props.deleteFriend(thisSpecificFriend, currentUser)
-                  }
-                  negative
-                  icon="checkmark"
-                  labelPosition="right"
-                  content="Remove Friend"
-                />
-              </Modal.Actions>
-            </Modal>
-          </h5>
-        </Message>
+        <Card.Group>
+          <Card key={this.props.user.id} className="card">
+            <h5 className="card-title">
+              <Image
+                floated="right"
+                size="mini"
+                src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+              />
+
+              <Card.Header>{this.props.user.username}</Card.Header>
+              <Card.Meta>You are friends!</Card.Meta>
+              <Button
+                size="tiny"
+                color="red"
+                onClick={() =>
+                  this.props.deleteFriend(thisSpecificFriend, currentUser)
+                }
+                className="card-link"
+              >
+                Delete Friend
+              </Button>
+              <Modal
+                dimmer={dimmer}
+                open={open}
+                onClose={this.close}
+                trigger={
+                  <Button size="tiny" color="purple" onClick={this.show(true)}>
+                    Details
+                  </Button>
+                }
+              >
+                <Modal.Header>Details</Modal.Header>
+                <Modal.Content image>
+                  <Image
+                    wrapped
+                    size="small"
+                    src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
+                  />
+                  <Modal.Description>
+                    <Header>{this.props.user.username}</Header>
+                    <p>This person is your friend!</p>
+                  </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button onClick={this.close} positive>
+                    Back
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      this.props.deleteFriend(thisSpecificFriend, currentUser)
+                    }
+                    negative
+                    icon="checkmark"
+                    labelPosition="right"
+                    content="Remove Friend"
+                  />
+                </Modal.Actions>
+              </Modal>
+            </h5>
+          </Card>
+        </Card.Group>
       </React.Fragment>
     );
   }
--- src/components/users/UsersCard.js
+++ src/components/users/UsersCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Message, Image, Modal, Header } from "semantic-ui-react";
+import { Button, Message, Image, Modal, Header, Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 export default class UsersCard extends Component {
@@ -13,60 +13,66 @@ export default class UsersCard extends Component {
 
     const { open, dimmer } = this.state;
     return (
-      <Message key={this.props.user.id} className="card">
-        <h5 className="card-title">
-          <Message.Header>
-            Not Friend's Name: {this.props.user.username}
-          </Message.Header>
-          <Button
-            size="tiny"
-            color="green"
-            onClick={() =>
-              this.props.addFriend(this.props.user.username, currentUser)
-            }
-            className="card-link"
-          >
-            Add Friend
-          </Button>
-          <Modal
-            dimmer={dimmer}
-            open={open}
-            onClose={this.close}
-            trigger={
-              <Button size="tiny" color="purple" onClick={this.show(true)}>
-                Details
-              </Button>
-            }
-          >
-            <Modal.Header>Details</Modal.Header>
-            <Modal.Content image>
-              <Image
-                wrapped
-                size="small"
-                src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
-              />
-              <Modal.Description>
-                <Header>{this.props.user.username}</Header>
-                <p>This person isn't your friend...</p>
-              </Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-              <Button onClick={this.close} negative>
-                Back
-              </Button>
-              <Button
-                onClick={() =>
-                  this.props.addFriend(this.props.user.username, currentUser)
-                }
-                positive
-                icon="checkmark"
-                labelPosition="right"
-                content="Add Friend"
-              />
-            </Modal.Actions>
-          </Modal>
-        </h5>
-      </Message>
+      <Card.Group>
+        <Card key={this.props.user.id} className="card">
+          <h5 className="card-title">
+            <Image
+              floated="right"
+              size="mini"
+              src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+            />
+            <Card.Header>{this.props.user.username}</Card.Header>
+            <Card.Meta>You are not currently friends.</Card.Meta>
+            <Button
+              size="tiny"
+              color="green"
+              onClick={() =>
+                this.props.addFriend(this.props.user.username, currentUser)
+              }
+              className="card-link"
+            >
+              Add Friend
+            </Button>
+            <Modal
+              dimmer={dimmer}
+              open={open}
+              onClose={this.close}
+              trigger={
+                <Button size="tiny" color="purple" onClick={this.show(true)}>
+                  Details
+                </Button>
+              }
+            >
+              <Modal.Header>Details</Modal.Header>
+              <Modal.Content image>
+                <Image
+                  wrapped
+                  size="small"
+                  src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
+                />
+                <Modal.Description>
+                  <Header>{this.props.user.username}</Header>
+                  <p>This person isn't your friend...</p>
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button onClick={this.close} negative>
+                  Back
+                </Button>
+                <Button
+                  onClick={() =>
+                    this.props.addFriend(this.props.user.username, currentUser)
+                  }
+                  positive
+                  icon="checkmark"
+                  labelPosition="right"
+                  content="Add Friend"
+                />
+              </Modal.Actions>
+            </Modal>
+          </h5>
+        </Card>
+      </Card.Group>
     );
   }
 }
